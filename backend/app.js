97a026bbf18797afb3579919227a86ba77bafda8
Flame787 <mbrezovic77@gmail.com>
2025-02-15 19:58:28 +0100

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,84 @@
+import fs from 'node:fs/promises';
+// file system
+
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {   // Middleware function starting on every comming HTTP request
+  res.setHeader('Access-Control-Allow-Origin', '*');  // enables access to API from all * domains
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');  // defines allowed methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  // enables sending 'Content-Type' headers = JSON data
+  next();
+});
+// CORS (Cross-Origin Resource Sharing) middleware in Express (when front- and backend are not on the same domain)
+
+app.get('/meals', async (req, res) => {
+  const meals = await fs.readFile('./data/available-meals.json', 'utf8');
+  res.json(JSON.parse(meals));
+});
+
+app.post('/orders', async (req, res) => {
+  const orderData = req.body.order;
+
+  if (orderData === null || orderData.items === null || orderData.items.length === 0) {
+    return res
+      .status(400)
+      .json({ message: 'Missing data.' });
+  }
+
+  if (
+    orderData.customer.email === null ||
+    !orderData.customer.email.includes('@') ||
+    orderData.customer.name === null ||
+    orderData.customer.name.trim() === '' ||
+    orderData.customer.street === null ||
+    orderData.customer.street.trim() === '' ||
+    orderData.customer['postal-code'] === null ||
+    orderData.customer['postal-code'].trim() === '' ||
+    orderData.customer.city === null ||
+    orderData.customer.city.trim() === ''
+  ) {
+    return res.status(400).json({
+      message:
+        'Missing data: Email, name, street, postal code or city is missing.',
+    });
+  }
+
+  const newOrder = {
+    ...orderData,
+    id: (Math.random() * 1000).toString(),
+  };
+  const orders = await fs.readFile('./data/orders.json', 'utf8');
+  const allOrders = JSON.parse(orders);
+  allOrders.push(newOrder);
+  await fs.writeFile('./data/orders.json', JSON.stringify(allOrders));
+  res.status(201).json({ message: 'Order created!' });
+});
+
+app.use((req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  res.status(404).json({ message: 'Not found' });
+});
+
+// app.listen(3000);
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
+
+
+/* 
+
+1. npm install  -> install dependencies for backend
+
+2. node app.js  -> to start the backend server
+
+*/
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <h1>You got this 💪</h1>
+      <p>Stuck? Not sure how to proceed?</p>
+      <p>Don't worry - we've all been there. Let's build it together!</p>
+    </>
+  );
+}
+
+export default App;
+
+
+/* 
+
+1. npm install  -> install dependencies for frontend
+
+2. npm run dev  -> to start the frontend server 
+
+*/
