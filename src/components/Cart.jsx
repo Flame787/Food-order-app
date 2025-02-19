2b595ac2f06ee511360ba9ffdc3df3f5e47a3ad7
Flame787 <mbrezovic77@gmail.com>
2025-02-19 17:05:30 +0100

    CartItem added, buttons inrease-decrease item quantity

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Modal from "./UI/Modal";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import CartItem from "./CartItem.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import UserProgressContext from "../store/UserProgressContext";
 
@@ -18,24 +19,37 @@ export default function Cart() {
   );
   // 0 - starting value
 
-  function handleCloseCart(){
+  function handleCloseCart() {
     userProgressCtx.hideCart();
   }
 
   return (
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
-        {/* when progress = 'cart', we want to show the Modal */}
+      {/* when progress = 'cart', we want to show the Modal */}
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          // <li key={item.id}>
+          //   {item.name} - {item.quantity}
+          // </li>
+
+          // <CartItem key={item.id} {...item} />
+          // we can spread the item-object onto the Cart, because item-object contains: name, quantity, price
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
